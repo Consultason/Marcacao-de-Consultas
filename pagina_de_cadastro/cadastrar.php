8ea927c3cf7b7b2a8d79411cd35d21f49cbaf16c
consultason <consultasprojeto@gmail.com>
2025-11-26 16:50:41 -0300

    banco de dados

---
+++ pagina_de_cadastro/cadastrar.php
@@ -0,0 +1,25 @@
+<?php
+// Conecta ao banco
+require_once "conexao.php";
+
+// Recebe os valores enviados pelo formulário
+$nome   = $_POST['nome'];
+$cpf    = $_POST['cpf'];
+$email  = $_POST['email'];
+$tel    = $_POST['tel'];
+$senha  = $_POST['senha'];
+
+// Prepara o comando SQL
+$sql = "INSERT INTO usuarios (nome, cpf, email, telefone, senha)
+        VALUES ('$nome', '$cpf', '$email', '$tel', '$senha')";
+
+if ($conn->query($sql) === TRUE) {
+    // Se cadastrar com sucesso
+    header("Location: ../pagina_de_login/login.html");
+    exit;
+} else {
+    echo "Erro ao cadastrar: " . $conn->error;
+}
+
+$conn->close();
+?>
--- pagina_de_cadastro/conexao.php
+++ pagina_de_cadastro/conexao.php
@@ -1,12 +1,22 @@
 <?php
+// Variável que armazena o endereço do servidor MySQL (normalmente localhost)
 $servidor = "localhost";
+
+// Usuário do banco de dados (padrão do XAMPP/WAMP é "root")
 $usuario = "root";
+
+// Senha do banco de dados (vazia por padrão no XAMPP/WAMP)
 $senha = "";
+
+// Nome do banco de dados que será utilizado
 $banco = "BemNordeste";
 
+// Cria a conexão com o banco usando a extensão MySQLi
 $conn = new mysqli($servidor, $usuario, $senha, $banco);
 
-if ($conn->connect_error) {
-    die("Erro na conexão com o banco: " . $conn->connect_error);
+// Verifica se ocorreu algum erro na conexão
+if($conn->connect_error){
+    // Encerra o script e exibe a mensagem de erro
+    die("Erro na conexão: " . $conn->connect_error);
 }
 ?>
